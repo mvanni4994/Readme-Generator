8d5c1369ca5f51b5cca069ad63f5229fb429e8c8
Matthew Vanni <mvanni4994@gmail.com>
2021-04-08 18:43:41 -0400

    added input fields

--- index.js
+++ index.js
@@ -2,7 +2,58 @@
 const fs = require('fs');
 const inquirer = require('inquirer')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: "input",
+    name: "title",
+    message: "Please input the title of your Project:",
+},
+{
+    type: "input",
+    name: "description",
+    message: "Please input a description of your Project:",
+},
+{
+    type: "input",
+    name: "installation",
+    message: "Please input any instructions on how to install:",
+},
+{
+    type: "input",
+    name: "usage",
+    message: "Please input usage information:",
+},
+{
+    type: "input",
+    name: "contributions",
+    message: "How would you like other devs to contribute to your application?",
+},
+{
+    type: "input",
+    name: "testing",
+    message: "Please provide instructions on how to use your application:",
+},
+{
+    type: "list",
+    name: "license",
+    message: "Which license would you like to select for your application?",
+    choices: ['MIT', 'AGPL','GPL'],
+},
+{
+    type: "input",
+    name: "github",
+    message: "Please input your github username:",
+},
+{
+    type: "input",
+    name: "email",
+    message: "Please input your email address:",
+},
+{
+    type: "input",
+    name: "repo",
+    message: "Please input the link to your repo:",
+},
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
